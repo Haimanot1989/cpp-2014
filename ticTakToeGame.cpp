a5f8ab729030afe73983ca8faa622e40c415536a
Haimanot Ftsumbrhan Tekie <haimif@yahoo.com>
2013-04-01 05:05:28 +0200

    gameklasse added

---
+++ ticTakToeGame.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "class_game.h"
+#include "ticTakToeGame.h"
+
+using namespace std;
+using namespace casino;
+
+tikTakToeGame::tikTakToeGame(int newPlayer = 1): game(ticTakToe){
+
+}
---
+++ ticTakToeState.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "class_gameState.h"
+#include "ticTakToeState.h"
+#include <vector>
+
+
+using namespace std;
+using namespace casino;
+
+ticTakToeState::ticTakToeState(vector<vector<unsigned int> > newPositionv, int newHero, int newMachine){
+
+	positionChose = newPositionv;
+	ourHero = newHero;
+	machine = newMachine;
+}
+
+void ticTakToeState::print(){
+}
